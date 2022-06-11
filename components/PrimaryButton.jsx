3990977a60719a6ae7376d10585bf1acfb8a1c52
arunabhg <arunabh.tech@gmail.com>
2022-06-11 23:14:07 +0530

    Initial commit

---
+++ components/PrimaryButton.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+
+const PrimaryButton = ({children}) => {
+  return (
+	<View>
+	  <Text>{children}</Text>
+	</View>
+  )
+}
+
+export default PrimaryButton;
