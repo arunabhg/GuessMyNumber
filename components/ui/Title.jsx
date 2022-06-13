7df30655eca2dc59a14259e77789e14c834ea810
arunabhg <arunabh.tech@gmail.com>
2022-06-14 00:07:28 +0530

    Moved to separate ui folder

---
+++ components/ui/Title.jsx
@@ -0,0 +1,22 @@
+import { Text, StyleSheet } from "react-native";
+
+import Colors from "../../constants/colors";
+
+function Title({ children }) {
+	return <Text style={styles.title}>{children}</Text>;
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+	title: {
+		fontSize: 24,
+		fontWeight: "bold",
+		color: "white",
+		textAlign: "center",
+		borderWidth: 2,
+		borderColor: "white",
+		padding: 12,
+		marginTop: 12
+	}
+});
