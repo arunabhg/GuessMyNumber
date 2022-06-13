7beb46facc5cf176d0e7a7c893f5207e5baad60a
arunabhg <arunabh.tech@gmail.com>
2022-06-13 20:37:45 +0530

    Pick colors from palette

--- components/PrimaryButton.jsx
+++ components/PrimaryButton.jsx
@@ -1,5 +1,7 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
+import Colors from "../constants/colors";
+
 function PrimaryButton({ children, onPressed }) {
 	return (
 		<View style={styles.buttonOuterContainer}>
@@ -10,7 +12,7 @@ function PrimaryButton({ children, onPressed }) {
 						: styles.buttonInnerContainer
 				}
 				onPress={onPressed}
-				android_ripple={{ color: "#640233" }}>
+				android_ripple={{ color: Colors.primary600 }}>
 				<Text style={styles.buttonText}>{children}</Text>
 			</Pressable>
 		</View>
@@ -26,7 +28,7 @@ const styles = StyleSheet.create({
 		overflow: "hidden"
 	},
 	buttonInnerContainer: {
-		backgroundColor: "#72063c",
+		backgroundColor: Colors.primary500,
 		paddingVertical: 8,
 		paddingHorizontal: 16,
 		elevation: 2
