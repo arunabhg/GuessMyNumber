80a5340218fde6b5e9f7813cf48c14c049408d36
arunabhg <arunabh.tech@gmail.com>
2022-06-13 20:38:25 +0530

    Added navigation & color palette

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -3,7 +3,9 @@ import { TextInput, View, StyleSheet, Alert } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+import Colors from "../constants/colors";
+
+function StartGameScreen({ onPickNumber }) {
 	const [enteredNumber, setEnteredNumber] = useState("");
 
 	function numberInputHandler(enteredText) {
@@ -27,7 +29,7 @@ function StartGameScreen() {
 			return;
 		}
 
-		console.log("Valid Number!");
+		onPickNumber(chosenNumber);
 	}
 
 	return (
@@ -62,7 +64,7 @@ const styles = StyleSheet.create({
 		marginTop: 100,
 		marginHorizontal: 24,
 		padding: 16,
-		backgroundColor: "#3b021f",
+		backgroundColor: Colors.primary800,
 		borderRadius: 8,
 		elevation: 4,
 		shadowColor: "black",
@@ -74,9 +76,9 @@ const styles = StyleSheet.create({
 		height: 50,
 		width: 50,
 		fontSize: 32,
-		borderBottomColor: "#ddb52f",
+		borderBottomColor: Colors.accent500,
 		borderBottomWidth: 2,
-		color: "#ddb52f",
+		color: Colors.accent500,
 		marginVertical: 8,
 		fontWeight: "bold",
 		textAlign: "center"
