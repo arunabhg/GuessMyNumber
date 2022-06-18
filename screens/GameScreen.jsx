51a8f0c66d8d6b0c867ecd337f6dd12980928359
arunabhg <arunabh.tech@gmail.com>
2022-06-18 23:33:59 +0530

    Added icons

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { View, StyleSheet, Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import Title from "../components/ui/Title";
 
@@ -72,12 +73,12 @@ function GameScreen({ userNumber, onGameOver }) {
 				<View style={styles.buttonsContainer}>
 					<View style={styles.buttonContainer}>
 						<PrimaryButton onPressed={nextGuessHandler.bind(this, "lower")}>
-							-
+							<Ionicons name="md-remove" size={24} color="white" />
 						</PrimaryButton>
 					</View>
 					<View style={styles.buttonContainer}>
 						<PrimaryButton onPressed={nextGuessHandler.bind(this, "greater")}>
-							+
+							<Ionicons name="md-add" size={24} color="white" />
 						</PrimaryButton>
 					</View>
 				</View>
