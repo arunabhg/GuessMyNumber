9a83f478507648856afe9f0db95ce46102146d2c
arunabhg <arunabh.tech@gmail.com>
2022-06-18 23:34:43 +0530

    Configured fonts & Splash screen

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
@@ -10,6 +12,16 @@ import Colors from "./constants/colors";
 export default function App() {
 	const [userNumber, setUserNumber] = useState();
 	const [gameOver, setGameOver] = useState(true);
+
+	// fontsLoaded is a boolean which returns whether the fonts have been loaded or not
+	const [fontsLoaded] = useFonts({
+		"open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+		"open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
 
 	function pickedNumberHandler(pickedNumber) {
 		setUserNumber(pickedNumber);
